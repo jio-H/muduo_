2dedd195954703aacb5a97d1246e1cb2a6586f42
jio-H <1463388724@qq.com>
2025-07-13 23:34:27 +0800

    finally-com

---
+++ include/InetAddress.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <string>
+
+// 封装socket地址类型
+class InetAddress
+{
+public:
+// 禁止隐式类型转换来调用构造函数
+    explicit InetAddress(uint16_t port = 0, std::string ip = "127.0.0.1");
+    explicit InetAddress(const sockaddr_in &addr)
+        : addr_(addr)
+    {
+    }
+
+    std::string toIp() const;
+    std::string toIpPort() const;
+    uint16_t toPort() const;
+
+    const sockaddr_in *getSockAddr() const { return &addr_; }
+    void setSockAddr(const sockaddr_in &addr) { addr_ = addr; }
+
+private:
+    sockaddr_in addr_;
+};
